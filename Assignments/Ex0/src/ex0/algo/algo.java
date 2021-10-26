55be9bfc2c6a2bbf26777544513a97e99ff3e4c0
barakdf <barakdf@gmail.com>
2021-10-26 17:49:44 +0300

    YALLA2

--- Assignments/Ex0/src/ex0/algo/algo.java
+++ Assignments/Ex0/src/ex0/algo/algo.java
@@ -11,6 +11,7 @@ public class algo implements ElevatorAlgo {
     final static int UP = 1, DOWN = -1;
     public Building building;
     public int direction;
+    int allo;
     ArrayList<FloorQueue> E_List;
 
 
@@ -18,6 +19,7 @@ public class algo implements ElevatorAlgo {
         this.building = b;
         E_List = new ArrayList<>();
         this.direction = UP;
+        allo = 0;
 
         for (int i = 0; i < b.numberOfElevetors(); i++) {
             FloorQueue e = new FloorQueue(this.building.getElevetor(i));
@@ -37,7 +39,30 @@ public class algo implements ElevatorAlgo {
 
     @Override
     public int allocateAnElevator(CallForElevator c) {
-        int ans = 0;
+        int ans = 1;
+        this.E_List.get(ans).setFloor(c.getSrc());
+        return ans;
+    }
+    private double dist(int src, int elev) {
+        double ans = -1;
+        Elevator thisElev = this.building.getElevetor(elev);
+        int pos = thisElev.getPos();
+        double speed = thisElev.getSpeed();
+        int min = this.building.minFloor(), max = this.building.maxFloor();
+        double up2down = (max-min)*speed;
+        double floorTime = speed+thisElev.getStopTime()+thisElev.getStartTime()+thisElev.getTimeForOpen()+thisElev.getTimeForClose();
+        if(elev%2==1) { // up
+            if(pos<=src) {ans = (src-pos)*floorTime;}
+            else {
+                ans = ((max-pos) + (pos-min))*floorTime + up2down;
+            }
+        }
+        else {
+            if(pos>=src) {ans = (pos-src)*floorTime;}
+            else {
+                ans = ((max-pos) + (pos-min))*floorTime + up2down;
+            }
+        }
         return ans;
     }
 
@@ -53,6 +78,16 @@ public class algo implements ElevatorAlgo {
 
     @Override
     public void cmdElevator(int elev) {
-        
+        Elevator curr = this.building.getElevetor(elev);
+        int pos = curr.getPos();
+        int first_F = E_List.get(elev).queue.getFirst();
+
+        if (curr.getState() == UP)
+        if (pos >= first_F) {
+            E_List.get(elev).queue.removeFirst();
+            curr.stop(first_F);
+        }
+        curr.goTo(E_List.get(elev).queue.getFirst());
+
     }
 }
