ee914c90bb6abe4750a3503928eca8ceb34c972d
YosefKahlon <yosefkahlon53@gmail.com>
2021-10-27 12:00:41 +0300

    add test package

---
+++ Test/ex0/algo/algoTest.java
@@ -0,0 +1,24 @@
+package ex0.algo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class algoTest {
+
+    @Test
+    void getBuilding() {
+    }
+
+    @Test
+    void algoName() {
+    }
+
+    @Test
+    void allocateAnElevator() {
+    }
+
+    @Test
+    void cmdElevator() {
+    }
+}
